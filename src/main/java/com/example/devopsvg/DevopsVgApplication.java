df4d61d1c87f1a7a60aa5707fd40ed12bad81bbd
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-09-11 09:08:04 +0200

    Initial commit

---
+++ src/main/java/com/example/devopsvg/DevopsVgApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsvg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsVgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsVgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devopsvg/DevopsVgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsvg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsVgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
