7426744c16453e1a1338c7aac33f9a75613679b1
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-09-11 12:28:10 +0200

    Removed redundant methods that did not override their superclasses, for now.

--- src/main/java/com/example/devopsvg/security/ConcreteUserDetails.java
+++ src/main/java/com/example/devopsvg/security/ConcreteUserDetails.java
@@ -34,20 +34,6 @@ public class ConcreteUserDetails implements UserDetails {
         return user.getUsername();
     }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
 
     @Override
     public boolean isEnabled() {
