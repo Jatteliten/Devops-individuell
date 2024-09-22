af1a71f8fd121de057dabdb8fae4e05aacaee110
Daniel Isaksson <Daniel.Isaksson@yh.Nackademin.se>
2024-09-22 22:03:06 +0200

    Added test that checks if PokemonController is null

--- src/test/java/com/example/devopsvg/DevopsVgApplicationTests.java
+++ src/test/java/com/example/devopsvg/DevopsVgApplicationTests.java
@@ -1,15 +1,22 @@
 package com.example.devopsvg;
 
+import com.example.devopsvg.controller.PokemonController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class DevopsVgApplicationTests {
 
+    @Autowired
+    private PokemonController pokemonController;
     @Test
     void contextLoads() {
+        assertThat(pokemonController).isNotNull();
     }
 
 }
